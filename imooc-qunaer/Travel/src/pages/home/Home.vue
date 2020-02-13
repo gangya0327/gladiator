11850bac3c50c6e50cfac1a840cc5f04a3d96085
gangya0327 <king9999427@163.com>
2020-02-14 07:26:47 +0800

    head

--- imooc-qunaer/Travel/src/pages/home/Home.vue
+++ imooc-qunaer/Travel/src/pages/home/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>home</div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div>text</div>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { HomeHeader, HomeSwiper }
 }
 </script>
 
---
+++ imooc-qunaer/Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt />
+      </swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+        },
+        {
+          id: '002',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/24/36/10/3fc94d042ae62de380581d4c98.jpg_92.jpg'
+        },
+        {
+          id: '003',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/ad/7c/fe/037d1a400372ff57b0030d1d1c.jpg_92.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  height: 0;
+  width: 100%;
+  padding-bottom: 31.25%;
+}
+
+.swiper-img {
+  width: 100%;
+}
+</style>
