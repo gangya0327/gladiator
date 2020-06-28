5ae14207b8a454b04d9f9d134899aaae66522acf
gangya0327 <king9999427@163.com>
2020-06-28 22:55:56 +0800

    redux

--- kaikeba10/react_base/react-redux-router/src/demo/Hooks.js
+++ kaikeba10/react_base/react-redux-router/src/demo/Hooks.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { loadingStart, loadingEnd, init, add, asyncFetch } from '../store'
+import { add, asyncFetch } from '../store/fruit.redux'
+import { login } from '../store/user.redux'
+import { BrowserRouter, Link, Route, Redirect, Switch } from 'react-router-dom'
 
 function FruitList({ fruits, setFruit }) {
   return (
-    fruits.map(f => (
-      <li key={f} onClick={() => setFruit(f)}>{f}</li>
-    ))
+    <div>
+      <ul>
+        {
+          fruits.map(f => (
+            <li key={f} onClick={() => setFruit(f)}>
+              <Link to={'/list/detail/' + f}>{f}</Link>
+            </li>
+          ))
+        }
+      </ul>
+      <Route path='/list/detail/:fruit' component={Detail}></Route>
+    </div>
   )
 }
 
@@ -24,6 +35,59 @@ function FruitAdd({ add }) {
   )
 }
 
+function Detail({ match, history, location }) {
+  console.log(match, history, location)
+  return (
+    <div>
+      <h3>{match.params.fruit}详情</h3>
+      <p>...</p>
+      <div>
+        <button onClick={history.goBack}>返回</button>
+      </div>
+    </div>
+  )
+}
+
+// 创建高阶组件，包装Route使其可以验证用户登录状态
+const PrivateRoute = connect(state => ({
+  isLogin: state.user.isLogin
+}))(function ({ component: Component, isLogin, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={
+        props => (
+          isLogin
+            ? (<Component {...props}></Component>)
+            : (<Redirect
+              to={{
+                pathname: '/login',
+                state: { redirect: props.location.pathname }
+              }}
+            ></Redirect>)
+        )
+      }
+    >
+    </Route>
+  )
+})
+
+const Login = connect(state => ({
+  isLogin: state.user.isLogin
+}), { login })(function ({ location, isLogin, login }) {
+  const redirect = location?.state?.redirect || '/'
+  if (isLogin) {
+    return <Redirect to={redirect} />
+  }
+  return (
+    <div>
+      <p>用户登录</p>
+      <hr />
+      <button onClick={login}>登录</button>
+    </div>
+  )
+})
+
 function HookTest({ fruits, loading, asyncFetch }) {
   const [fruit, setFruit] = useState('草莓')
   useEffect(() => {
@@ -36,26 +100,40 @@ function HookTest({ fruits, loading, asyncFetch }) {
   }, [fruit])
 
   return (
-    <div>
-      <p>{fruit === '' ? '请选择喜欢的水果' : `你选择了${fruit}`}</p>
-      <FruitAdd />
-      {
-        loading
-          ? '加载中...' :
-          <FruitList fruits={fruits} setFruit={setFruit} />
-      }
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to='/list'>水果列表</Link>
+        |
+        <Link to='/add'>添加水果</Link>
+      </nav>
+      <div>
+        <Switch>
+          <Route path='/list' render={() => (
+            loading
+              ? '加载中...' :
+              <FruitList fruits={fruits} setFruit={setFruit} />
+
+          )}></Route>
+          {/* <Route exact path='/add' component={FruitAdd}></Route> */}
+          <PrivateRoute exact path='/add' component={FruitAdd}></PrivateRoute>
+          <Route to='/login' component={Login}></Route>
+          {/* <Redirect to='/list'></Redirect> */}
+          <Route component={() => (<h2>页面不存在</h2>)}></Route>
+        </Switch>
+      </div>
+      <div>
+        <p>{fruit === '' ? '请选择喜欢的水果' : `你选择了${fruit}`}</p>
+        {/* <FruitAdd /> */}
+      </div>
+    </BrowserRouter>
   )
 }
 
 const mapStateToProps = state => ({
-  fruits: state.list,
-  loading: state.loading
+  fruits: state.fruit.list,
+  loading: state.fruit.loading
 })
 const mapDispatchToProps = {
-  loadingStart,
-  loadingEnd,
-  init,
   add,
   asyncFetch
 }
--- kaikeba10/react_base/react-redux-router/src/store/index.js
+++ kaikeba10/react_base/react-redux-router/src/store/index.js
@@ -1,51 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
-
-export const init = (payload) => ({
-  type: 'init',
-  payload
-})
-export const add = (payload) => ({
-  type: 'add',
-  payload
-})
-export const loadingStart = () => ({
-  type: 'loading_start'
-})
-export const loadingEnd = () => ({
-  type: 'loading_end'
-})
-
-export const asyncFetch = payload => {
-  return dispatch => {
-    dispatch({ type: 'loading_start' })
-    setTimeout(() => {
-      dispatch({ type: 'loading_end' })
-      dispatch({ type: 'init', payload })
-    }, 1000);
-  }
-}
-
-// 将状态移至全局
-function fruitReducer(state = {
-  list: [],
-  loading: false
-}, action) {
-  switch (action.type) {
-    case 'init':
-      return { ...state, list: action.payload }
-    case 'add':
-      return { ...state, list: [...state, action.payload] }
-    case 'loading_start':
-      return { ...state, loading: true }
-    case 'loading_end':
-      return { ...state, loading: false }
-    default:
-      return state
-  }
-}
+import fruitReducer from './fruit.redux'
+import userReducer from './user.redux'
 
 // 第二个参数是中间件函数
-const store = createStore(fruitReducer, applyMiddleware(logger, thunk))
+const store = createStore(
+  combineReducers({ fruit: fruitReducer, user: userReducer }),
+  applyMiddleware(logger, thunk)
+)
 export default store
