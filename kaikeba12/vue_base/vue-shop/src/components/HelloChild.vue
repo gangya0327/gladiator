b274131c7a3d4399a9e2160b9068fbe7bfa8244b
gangya0327 <king9999427@163.com>
2020-08-24 01:11:37 +0800

    none

---
+++ kaikeba12/vue_base/vue-shop/src/components/HelloChild.vue
@@ -0,0 +1,35 @@
+<template>
+  <div @click="$parent.$emit('bi')">
+    <span @click="$emit('foo','abc')">{{ msg || 'hello' }}, {{ text }}</span> |
+    <span>{{ grand }}</span>
+    <p>匿名插槽：<slot /></p>
+    <p>具名插槽：<slot name="abc" /></p>
+    <p>作用域插槽：<slot name="abcd" bla="blaText" /></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    },
+  },
+  inject: ['grand'],
+  data() {
+    return {
+      text: '世界'
+    }
+  },
+  created() {
+    this.$parent.$on('bi', () => {
+      console.log('bibibi')
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
